533a2477c07cac2a063451b9e0a1eef58eec10f0
Olexandra Ghordievska <maynarisochka@gmail.com>
2025-04-24 16:01:36 +0300

    check env file and add libraries

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import setupServer from './server.js';
 import initMongoConnection from './db/initMongoConnection.js';
 
 const startServer = async () => {
-    await initMongoConnection();
+  await initMongoConnection();
 
-    const app = setupServer();
-    const PORT = process.env.PORT || 3000;
+  const app = setupServer();
+  const PORT = process.env.PORT || 3000;
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
 
 startServer();
