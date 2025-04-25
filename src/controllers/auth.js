9c3435ed5f2f3f8b59a663757e27f24ab2ed0471
Olexandra Ghordievska <maynarisochka@gmail.com>
2025-04-25 10:48:38 +0300

    add registration message

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -27,7 +27,7 @@ export const register = async (req, res, next) => {
     res.status(201).json({
       status: 201,
       message: 'Successfully registered a user!',
-      data: { id: newUser._id, name: newUser.name, email: newUser.email },
+      user: { id: newUser._id, name: newUser.name, email: newUser.email },
     });
   } catch (error) {
     next(error);
@@ -169,68 +169,4 @@ export const logout = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-};
-
-import {
-  loginUser,
-  refreshUsersSession,
-  registerUser,
-} from '../services/auth.js';
-
-export const registerUserController = async (req, res) => {
-  const user = await registerUser(req.body);
-
-  res.status(201).json({
-    status: 201,
-    message: 'Successfully registered a user!',
-    data: user,
-  });
-};
-
-export const loginUserController = async (req, res) => {
-  const session = await loginUser(req.body);
-
-  res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 день в миллисекундах
-  });
-
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 день в миллисекундах
-  });
-
-  res.json({
-    status: 200,
-    message: 'Successfully logged in an user!',
-    data: session.accessToken,
-  });
-};
-
-export const setupSession = (res, session) => {
-  res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 день в миллисекундах
-  });
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 день в миллисекундах
-  });
-};
-
-export const refreshUserSessionController = async (req, res) => {
-  const session = await refreshUsersSession({
-    sessionId: req.cookies.sessionId,
-    refreshToken: req.cookies.refreshToken,
-  });
-
-  setupSession(res, session);
-
-  res.json({
-    status: 200,
-    message: 'Successfully refreshed a session!',
-    data: {
-      accessToken: session.accessToken,
-    },
-  });
 };
