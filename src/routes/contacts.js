7203b751ea1c1f9b8594485a9091bf3314b311d3
Olexandra Ghordievska <maynarisochka@gmail.com>
2025-05-07 15:02:33 +0300

    change routes

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -18,6 +18,7 @@ import { upload } from '../middlewares/upload.js';
 
 const router = express.Router();
 
+
 router.use(authMiddleware);
 router.get('/', ctrlWrapper(getAllContacts));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
